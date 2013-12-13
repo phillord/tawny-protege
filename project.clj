172e4fdee2556d512f72af332a3734e44eac1532
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-12-13 08:22:28 +0000

    Prepare for next iteration. Added SCM information.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject uk.org.russet/tawny-protege "1.0.0"
+(defproject uk.org.russet/tawny-protege "1.1.0-SNAPSHOT"
   :description "Link between Tawny-OWL and Protege"
     :license {:name "LGPL"
             :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
             :distribution :repo}
   :url "https://github.com/phillord/tawny-protege"
+  :scm {:url "https://github.com/phillord/tawny-protege"
+        :name "git"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [uk.org.russet/tawny-owl "1.0"]
                  [uk.org.russet/nrepl-clojure "1.0.0"]])
